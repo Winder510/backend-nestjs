526c3b29e73dcfb7ab3e0a1d12d7df5b4a9e8a98
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2024-08-17 23:14:08 +0700

    update user schema and validate dto for user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,68 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsDefined,
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
 
-export class CreateUserDto {
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-  @IsNotEmpty()
-  password: string;
+class Company {
+  @IsNotEmpty({ message: 'id không được để trống' })
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty({ message: 'name không được để trống' })
   name: string;
+}
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name không được để trống ' })
+  name: string;
+
+  @IsEmail({ message: 'email không đúng định dạng' })
+  @IsNotEmpty({ message: 'email không được để trống' })
+  email: string;
+
+  @IsNotEmpty({ message: 'password không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'age không được để trống' })
+  age: string;
+
+  @IsNotEmpty({ message: 'gender không được để trống' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'address không được để trống' })
+  address: string;
+
+  @IsNotEmpty({ message: 'role không được để trống' })
+  role: string;
+
+  @IsDefined()
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name không được để trống ' })
+  name: string;
+
+  @IsEmail({ message: 'email không đúng định dạng' })
+  @IsNotEmpty({ message: 'email không được để trống' })
+  email: string;
+
+  @IsNotEmpty({ message: 'password không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'age không được để trống' })
+  age: string;
+
+  @IsNotEmpty({ message: 'gender không được để trống' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'address không được để trống' })
   address: string;
 }
